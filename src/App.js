c97eac1c6f4f1693d2e33be30bb322139c66a4eb
Wynston <houyewei@gmail.com>
2021-11-08 23:26:42 +0800

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// CSS
-import './App.css';
 // websocket context provider
 import WebSocketProvider from './features/websocket/WebSocketProvider';
 // game player rank list component
 import { RankList } from './features/rank/RankList'
 // Websocket component status component
 import { WebSocket } from './features/websocket/WebSocket'
+// CSS
+import './App.css';
 
 // root app
 function App() {
